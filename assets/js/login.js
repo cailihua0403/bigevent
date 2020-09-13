49462e5d80fac47ca572284082a3b57f6449bf82
cailihua <1500615775@qq.comgit config --global user.email 1500615775@qq.com>
2020-09-13 21:23:57 +0800

    表单验证

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,4 +7,13 @@ $(function(){
         $('.login-box').show()
         $('.reg-box').hide()
     })
+   var form=layui.form
+   form.verify({
+       pwd:[/^[\S]{6,12}$/,'密码应为6-12位'],
+       repwd:function(value){
+        if($('.reg-box [name=password]').val()!==value) {
+            return '两次密码不一致' 
+        }
+       }
+   })
 })
